ed41991dc3d373bcfdf3a3b47f9691192dcbb6d6
Stephen MacKenzie <stevemac321@live.com>
2024-07-18 06:24:22 +0000

    open_table builds and runs
    need to make sure the results are correct and that the addressing is efficient.

---
+++ opentable/main.c
@@ -0,0 +1,140 @@
+/*==============================================================================
+ Name        : main.c
+ Author      : Stephen MacKenzie
+ Copyright   : Licensed under GPL version 2 (GPLv2)
+==============================================================================*/
+
+#include "common.h"
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wincompatible-pointer-types"
+
+#ifndef NL
+#define NL printf("\n")
+#endif
+
+#define SINE_ARRAY_SIZE 91
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
+/* TEST HARNESS*/
+int passed = 0;
+int failed = 0;
+int tcs = 0;
+
+int int_cmp(const int* v1, const int* v2);
+void print_int_array(int *arr, const size_t count);
+void table_test1();
+void table_test2();
+void sine_table_test();
+
+int main()
+{
+	Heap_Init();
+	enable_virtual_terminal_processing();
+
+	table_test1();
+    table_test2();
+	sine_table_test();
+}
+/*-----------------------------------------------------------------------------
+Test functions and data
+-----------------------------------------------------------------------------*/
+void table_test1()
+{
+	
+	TC_BEGIN(__func__);
+	tableptr table = open_table_init(97, sizeof(int), inthash, inthash, int_cmp, table_int_alloc);
+	VERIFY(table);
+	PASSED(__func__, __LINE__);
+
+	int a[97];
+	int dupes = 0;
+	modify(a, _countof(a), sizeof(int), rand_int);
+	print_int_array(a, _countof(a));
+	for (int i = 0; i < _countof(a); i++) {
+		if (EXISTS == open_table_insert(table, &a[i]))
+			dupes++;
+	}
+
+	open_table_visit(table, print_int);
+	NL;
+	table_report(table);
+	printf("dupes: %d\n", dupes);
+	open_table_destroy(table);
+	PASSED(__func__, __LINE__);
+	
+}
+void table_test2()
+{
+	TC_BEGIN(__func__);
+	tableptr table = open_table_init(97, sizeof(const char*), strhash, strhash, table_str_cmp, table_str_alloc);
+	VERIFY(table);
+	PASSED(__func__, __LINE__);
+	
+	char *a[] = {"John",  "Mary",  "Joseph", "Abraham",
+		     "Obama", "Satan", "Larry"};
+	size_t len = _countof(a);
+
+	for (int i = 0; i < len; i++)
+		open_table_insert(table, a[i]);
+
+	open_table_visit(table, print_str);
+	NL;
+	char *p = open_table_lookup(table, a[3]);
+	if (p)
+		printf("found: %s\n", p);
+
+	open_table_remove_entry(table, a[3]);
+	open_table_visit(table, print_str);
+	NL;
+	open_table_destroy(table);
+	PASSED(__func__, __LINE__);
+}
+void sine_table_test()
+{
+	TC_BEGIN(__func__);
+	tableptr table = open_table_init(SINE_ARRAY_SIZE * 2, sizeof(int), 
+						inthash, inthash, int_cmp, table_int_alloc);
+	VERIFY(table);
+	
+	int compare[SINE_ARRAY_SIZE];
+	float sf=0;
+	int res=0;
+	/* this loop taken from Zhu textbook */
+	for(int i=0; i < SINE_ARRAY_SIZE; i++) {
+		sf = sin(M_PI * i / 180);
+		res = (1 + sf) * 2048;
+		if(res == 0x1000)
+			res = 0xFFF;
+		
+		compare[i] = res;
+		open_table_insert(table, &res);
+	}
+	printf("contexts: \n");
+	open_table_visit(table, print_hex);
+	NL;
+	printf("compare: \n");
+	for(int i=0; i < SINE_ARRAY_SIZE; i++)
+		printf("%X ", compare[i]);
+	NL;
+	for(int i=0; i < SINE_ARRAY_SIZE; i++) {
+		int *p = open_table_lookup(table, &compare[i]);
+		VERIFY(p != NULL);
+	}
+
+	open_table_destroy(table);
+}
+// often used print integer array
+
+void print_int_array(int *arr, const size_t count)
+{
+	for (int *p = arr; p < arr + count; p++)
+		printf("%d ", *(int *)p);
+
+	printf("\n");
+}
+
+
+#pragma GCC diagnostic pop
---
+++ opentable/open_table.c
@@ -0,0 +1,239 @@
+/*==============================================================================
+ Name        : open_table.c 
+ Author      : Stephen MacKenzie
+ Copyright   : Licensed under GPL version 2 (GPLv2)
+==============================================================================*/
+#include "common.h"
+/**=============================================================================
+ Interface:   open_table
+
+ Purpose:    open_table is an open addressing hash table that uses the static
+             heap manager.  It uses double hashing for collision resolution.
+==============================================================================*/
+struct open_table {
+	size_t count;
+	size_t table_size;
+	size_t datasize;
+	size_t (*h1)(const genptr value);
+	size_t (*h2)(const genptr value);
+	int (*compare)(const genptr, const genptr);
+        void(*alloc_insert)(tableptr, size_t, genptr);
+	genptr* table;
+};
+
+/**=============================================================================
+ Function:   open_table_init
+
+ Purpose:    Returns an open_table interface pointer.  
+
+ Parameters: tbsize: sizeof the hash table, should be prime number.
+             datasize: size in bytes of an entry.
+             h1: primary hash function.
+	     h2: secondary hash function (can be the same as h1).
+	     compare: compare two generic types.
+
+ Returns:     open_table interface pointer.
+
+ Example:     tableptr tp = open_table_init(97, sizeof(foo), foo_h1, foo_h2);
+==============================================================================*/
+tableptr open_table_init(const size_t tbsize, const size_t datasize,
+			size_t (*h1)(const genptr),
+			size_t (*h2)(const genptr),
+			int (*compare)(const genptr, const genptr),
+                        void(*alloc_insert)(tableptr, size_t, genptr))
+{
+	assert(h1 && h2 && compare);
+	tableptr newtable = Heap_Malloc(sizeof(open_table));
+	if (newtable == NULL) {
+		fprintf(stderr, "out of memory error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	newtable->table = Heap_Malloc(tbsize * CHAR_BIT);
+	if (newtable->table == NULL) {
+		fprintf(stderr, "out of memory error\n");
+		free(newtable);
+		exit(EXIT_FAILURE);
+	}
+
+	newtable->table_size = tbsize;
+	newtable->datasize = datasize;
+	newtable->h1 = h1;
+	newtable->h2 = h2;
+	newtable->count=0;
+	newtable->compare = compare;
+        newtable->alloc_insert = alloc_insert;
+	newtable->count = 0;
+
+	for (int i = 0; i < tbsize; i++)
+		newtable->table[i] = NULL;
+
+	return newtable;
+}
+
+void open_table_destroy(tableptr table)
+{
+	assert(table);
+	for (int i = 0; i < table->table_size; i++) {
+		Heap_Free(table->table[i]);
+		table->table[i] = NULL;
+	}
+
+	Heap_Free(table->table);
+	Heap_Free(table);
+}
+/**=============================================================================
+ Function:   open_table_insert
+
+ Purpose:    Inserts a generic type into the hash table  
+
+ Parameters: table: open_table interface pointer
+             value: entry to add
+
+ Returns:     enum: INSERTED indicates that the new entry was added. EXISTS
+              indicates that the entry already exists in the table. FULL 
+	      indicates that there is no room for another entry, the table needs
+	      to be resized and rehashed.
+
+ Example:     open_table_insert(pt, &foo);
+==============================================================================*/
+enum insert_state open_table_insert(tableptr table, const genptr value)
+{
+	assert(table);
+
+	if (open_table_lookup(table, value) != NULL)
+		return EXISTS;
+
+	for (int i = 0; i < table->table_size; i++) {
+		size_t idx =
+		    (table->h1(value) + (i * table->h2(value))) % table->table_size;
+
+		if (table->table[idx] == NULL) {
+                        table->alloc_insert(table, idx, value);
+        		table->count++;
+			return INSERTED;
+		}
+	}
+	// else we could not find a slot!
+	return TABLEFULL;
+}
+_Bool open_table_remove_entry(tableptr table, const genptr value)
+{
+	assert(table);
+	for (int i = 0; i < table->table_size; i++) {
+		size_t idx =
+		    (table->h1(value) + (i * table->h2(value))) % table->table_size;
+
+		if (table->table[idx] != NULL &&
+		    (table->compare(value, table->table[idx]) == 0)) {
+
+			Heap_Free(table->table[idx]);
+			table->table[idx] = NULL;
+			table->count--;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+genptr open_table_lookup(const tableptr table, const genptr value)
+{
+	assert(table);
+	for (int i = 0; i < table->table_size; i++) {
+		size_t idx =
+		    (table->h1(value) + (i * table->h2(value))) % table->table_size;
+
+		if (table->table[idx] != NULL &&
+		    (table->compare(value, table->table[idx]) == 0))
+			return table->table[idx];
+	}
+	return NULL;
+}
+
+size_t open_table_resize(tableptr table, const size_t newsize)
+{
+	assert(table);
+	return table->table_size;
+}
+
+void open_table_visit(const tableptr table, void (*visit)(const genptr))
+{
+	assert(table);
+	for (int i = 0; i < table->table_size; i++)
+		if(table->table[i] != NULL)
+			visit(table->table[i]);
+}
+
+size_t open_table_size(const tableptr table)
+{
+	assert(table);
+	return table->table_size;
+}
+
+size_t open_table_entries(const tableptr table)
+{
+	assert(table);
+	return table->count;
+}
+_Bool open_table_resize_needed(const tableptr table)
+{
+	assert(table);
+	size_t cap = table->table_size - table->count;
+	if (cap < (table->table_size / 4))
+		return true;
+
+	return false;
+}
+
+size_t strhash(const char* value)
+{
+	assert(value);
+	const char *ptr = value;
+	size_t val = 0;
+	size_t tmp = 0;
+
+	val = (val << 4) + (*ptr);
+
+	if ((tmp = (val & 0xf0000000))) {
+		val = val ^ (tmp >> 24);
+		val = val ^ tmp;
+	}
+
+	return val;
+}
+size_t inthash(const int* value)
+{
+	assert(value);
+	const int* ptr = value;
+	size_t val = 0;
+	size_t tmp = 0;
+
+	val = (val << 4) + (*ptr);
+
+	if ((tmp = (val & 0xf0000000))) {
+		val = val ^ (tmp >> 24);
+		val = val ^ tmp;
+	}
+
+	return val;
+}
+void table_report(const tableptr table)
+{
+	printf("Table size: %zu Element count: %zu\n", table->table_size, 
+							table->count);
+}
+void table_int_alloc(tableptr table, size_t idx, genptr value)
+{
+        assert(table && value);
+        table->table[idx] = Heap_Malloc(table->datasize);
+	memcpy(table->table[idx], value, table->datasize);
+}
+
+void table_str_alloc(tableptr table, size_t idx, genptr value)
+{
+        assert(table && value);
+        const char * s = (const char*)value;
+        table->table[idx] = Heap_Malloc(strlen(s));
+	strcpy(table->table[idx], s);
+}
