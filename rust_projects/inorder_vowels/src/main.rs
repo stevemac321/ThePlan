77ac3aaa208eacbfb32da9942cf3686ecf5fffb9
stevemac321 <stevemac321@live.com>
2024-08-08 23:59:19 -0700

    added more statemachine studies

--- rust_projects/inorder_vowels/src/main.rs
+++ rust_projects/inorder_vowels/src/main.rs
@@ -45,8 +45,7 @@ fn inorder_vowels(words: &[char]) -> (i32, i32) {
 
     (count, total)
 }
-
-fn main() {
+fn test_inorder_vowels() {
     let words: [char; 68] = ['a', 'b', 's', 't', 'e', 'm', 'i', 'o', 'u', 's', ' ', 'f', 'a', 'c', 'e', 't', 'i',
     'o', 'u', 's', ' ', 'a', 'e', 'r', 'i', 'f', 'o', 'r', 'm', ' ', 'a', 'r', 's', 'e', 'n', 'i', 'o', 'u', 's',
     ' ', 'a', 'n', 'e', 'm', 'i', 'o', 'u', 's', ' ', 'c', 'a', 'e', 's', 'i', 'u', 'm', ' ', 'b', 'a', 'c', 'o',
@@ -54,4 +53,66 @@ fn main() {
 
     let (count, total) = inorder_vowels(&words);
     println!("Words with vowels in order: {}/{}", count, total);
+}
+
+use std::fs::File;
+use std::io::{self, Read, Write, BufReader, BufWriter};
+use std::path::Path;
+
+#[derive(PartialEq)]
+enum CommentState {
+    Normal,
+    MaybeComment,
+    InComment,
+    MaybeEndComment,
+}
+
+fn remove_comments() -> io::Result<()> {
+    let input_path = Path::new("btree.h");
+    let output_path = Path::new("btree_no_comments.h");
+
+    let input_file = File::open(input_path)?;
+    let mut reader = BufReader::new(input_file);
+    let output_file = File::create(output_path)?;
+    let mut writer = BufWriter::new(output_file);
+
+    let mut input = String::new();
+    reader.read_to_string(&mut input)?;
+
+    let mut state = CommentState::Normal;
+    let mut output = String::new();
+
+    for c in input.chars() {
+        state = match (state, c) {
+            (CommentState::Normal, '/') => CommentState::MaybeComment,
+            (CommentState::Normal, _) => {
+                output.push(c);
+                CommentState::Normal
+            },
+            (CommentState::MaybeComment, '*') => CommentState::InComment,
+            (CommentState::MaybeComment, _) => {
+                output.push('/');
+                output.push(c);
+                CommentState::Normal
+            },
+            (CommentState::InComment, '*') => CommentState::MaybeEndComment,
+            (CommentState::InComment, _) => CommentState::InComment,
+            (CommentState::MaybeEndComment, '/') => CommentState::Normal,
+            (CommentState::MaybeEndComment, '*') => CommentState::MaybeEndComment,
+            (CommentState::MaybeEndComment, _) => CommentState::InComment,
+        };
+    }
+
+    // Handle any trailing '/' if we ended in MaybeComment state
+    if state == CommentState::MaybeComment {
+        output.push('/');
+    }
+
+    writer.write_all(output.as_bytes())?;
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    inorder_vowels();
+    remove_comments()
 }
